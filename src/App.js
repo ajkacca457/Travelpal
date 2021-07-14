eb3116b500c850255d4f4916a21c0f1a38afe074
Avijit Karmaker <ajkacca@gmail.com>
2021-07-15 01:35:35 +0300

    add essential components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
+import Services from './pages/Services';
 import Singleroom from './pages/Singleroom';
 import Singleservice from './pages/Singleservice';
+import Error from './pages/Error';
+import Nav from './componets/Nav';
 
 function App() {
   return (
     <div className="App">
-
+      <Nav></Nav>
       <Home></Home>
       <Rooms></Rooms>
+      <Services></Services>
       <Singleroom></Singleroom>
       <Singleservice></Singleservice>
+      <Error></Error>
    
     </div>
   );
